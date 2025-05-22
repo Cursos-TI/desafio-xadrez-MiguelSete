f8352a26f893298c189b53b0f13a5040e10159c0
MiguelSete <127905385+MiguelSete@users.noreply.github.com>
2025-05-22 17:17:26 +0000

    Nivel Aventureiro concluido

--- xadrez.c
+++ xadrez.c
@@ -12,17 +12,17 @@ int main() {
         printf("Direita\n");
     }
 
-    printf("\n"); // Separação visual
+    printf("\n"); 
 
     // Movimento do Bispo: 5 casas na diagonal superior direita
     printf("Movimento do Bispo:\n");
     int passoBispo = 1;
     while (passoBispo <= MOV_BISPO) {
-        printf("Cima Direita\n"); // Combinação de direções básicas
+        printf("Cima Direita\n"); 
         passoBispo++;
     }
 
-    printf("\n"); // Separação visual
+    printf("\n");
 
     // Movimento da Rainha: 8 casas para a esquerda
     printf("Movimento da Rainha:\n");
@@ -32,5 +32,26 @@ int main() {
         passoRainha++;
     } while (passoRainha <= MOV_RAINHA);
 
+
+    const int movimentosBaixo = 2;
+    const int movimentosEsquerda = 1;
+
+    printf("Movimento do Cavalo:\n");
+
+    for (int i = 1; i <= movimentosBaixo; i++) {
+        printf("Baixo\n");
+
+        // Loop while aninhado
+        int controle = 1;
+        while (controle <= 1 && i == movimentosBaixo) {
+            // Só executa a esquerda no fim do segundo "Baixo"
+            for (int j = 1; j <= movimentosEsquerda; j++) {
+                printf("Esquerda\n");
+            }
+            controle++;
+        }
+    }
+
     return 0;
 }
+
